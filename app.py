026cfef708dbbdeb1bac5c52219742cf62a7f0c0
inyomansetiawan <insetiawan98@gmail.com>
2024-12-18 20:43:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("path_to_your_credentials_file.json")
+cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
 
 # Dummy data untuk login dan tim terkait
